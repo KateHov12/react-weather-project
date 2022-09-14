1b6699f8123249fec06679e2e2863482ef43dda7
Kateryna Hovhannisyan <56844857+KateHov12@users.noreply.github.com>
2022-09-14 12:53:17 +0300

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by KateHov and is{" "}
+        <a
+          href="https://github.com/KateHov12/react-weather-project"
+          target="blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
   );
 }
 
